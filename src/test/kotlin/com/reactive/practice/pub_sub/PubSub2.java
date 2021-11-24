b13a753f6d883332175a0ccd520e6ba3adb4a399
icarus8050 <icarus8050@naver.com>
2021-11-25 01:10:40 +0900

    mapPub

--- src/test/kotlin/com/reactive/practice/pub_sub/PubSub2.java
+++ src/test/kotlin/com/reactive/practice/pub_sub/PubSub2.java
@@ -1,43 +1,42 @@
 package com.reactive.practice.pub_sub;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
 public class PubSub2 {
     public static void main(String[] args) {
-        Publisher<Integer> pub = new Publisher<>() {
-            Iterable<Integer> iter = Stream.iterate(1, a -> a + 1)
-                    .limit(10)
-                    .collect(Collectors.toList());
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList()));
+        Publisher<Integer> mapPub = mapPub(pub, s -> s * 10);
+        Publisher<Integer> map2Pub = mapPub(mapPub, s -> s * 10);
+        map2Pub.subscribe(logSub());
+    }
 
+    private static Publisher<Integer> mapPub(Publisher<Integer> pub, Function<Integer, Integer> f) {
+        return new Publisher<Integer>() {
             @Override
             public void subscribe(Subscriber<? super Integer> sub) {
-                sub.onSubscribe(new Subscription() {
+                pub.subscribe(new DelegateSub(sub) {
                     @Override
-                    public void request(long n) {
-                        try {
-                            iter.forEach(a -> sub.onNext(a));
-                            sub.onComplete();
-                        } catch (Throwable t) {
-                            sub.onError(t);
-                        }
-                    }
-
-                    @Override
-                    public void cancel() {
-
+                    public void onNext(Integer i) {
+                        super.onNext(f.apply(i));
                     }
                 });
             }
         };
+    }
 
-        Subscriber<Integer> sub = new Subscriber<>() {
+    @NotNull
+    private static Subscriber<Integer> logSub() {
+        return new Subscriber<>() {
             @Override
             public void onSubscribe(Subscription s) {
                 log.debug("onSubscribe:");
@@ -59,7 +58,25 @@ public class PubSub2 {
                 log.debug("onComplete");
             }
         };
+    }
 
-        pub.subscribe(sub);
+    @NotNull
+    private static Publisher<Integer> iterPub(final List<Integer> iter) {
+        return sub -> sub.onSubscribe(new Subscription() {
+            @Override
+            public void request(long n) {
+                try {
+                    iter.forEach(sub::onNext);
+                    sub.onComplete();
+                } catch (Throwable t) {
+                    sub.onError(t);
+                }
+            }
+
+            @Override
+            public void cancel() {
+
+            }
+        });
     }
 }
