3835085921d2020859c7d17ffa51f090dd61206e
icarus8050 <icarus8050@naver.com>
2021-12-01 01:09:52 +0900

    Simple FutureTask Ex

--- src/test/kotlin/com/reactive/practice/future/FutureEx.kt
+++ src/test/kotlin/com/reactive/practice/future/FutureEx.kt
@@ -2,8 +2,9 @@ package com.reactive.practice.future
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.concurrent.Callable
 import java.util.concurrent.Executors
-import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
 
 val logger: Logger = LoggerFactory.getLogger(FutureEx::class.java)
 
@@ -12,14 +13,25 @@ class FutureEx
 fun main() {
     val es = Executors.newCachedThreadPool()
 
-    val f: Future<String> = es.submit<String> {
+    val f = object : FutureTask<String>(
+        Callable {
+            Thread.sleep(2000)
+            logger.info("Async")
+            "Hello"
+        }
+    ) {
+        override fun done() {
+            println(get())
+        }
+    }
+
+    /*val f: Future<String> = es.submit<String> {
         Thread.sleep(2000)
         logger.info("Async")
         return@submit "Hello"
-    }
-    logger.info(f.isDone.toString())
+    }*/
+
+    es.execute(f)
     logger.info("Exit")
-    logger.info(f.get())
-    logger.info(f.isDone.toString())
     es.shutdown()
 }
