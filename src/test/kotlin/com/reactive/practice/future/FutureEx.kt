325d3dc5ac937c3c5769dd1a48943ca09b5eb65c
icarus8050 <icarus8050@naver.com>
2021-12-01 02:11:24 +0900

    Simple Callback Ex

--- src/test/kotlin/com/reactive/practice/future/FutureEx.kt
+++ src/test/kotlin/com/reactive/practice/future/FutureEx.kt
@@ -3,6 +3,7 @@ package com.reactive.practice.future
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Callable
+import java.util.concurrent.ExecutionException
 import java.util.concurrent.Executors
 import java.util.concurrent.FutureTask
 
@@ -10,10 +11,47 @@ val logger: Logger = LoggerFactory.getLogger(FutureEx::class.java)
 
 class FutureEx
 
+fun interface SuccessCallback {
+    fun onSuccess(result: String)
+}
+
+fun interface ExceptionCallback {
+    fun onError(t: Throwable?)
+}
+
+class CallbackFutureTask(
+    callable: Callable<String>,
+    val sc: SuccessCallback,
+    val ex: ExceptionCallback,
+    ) : FutureTask<String>(callable) {
+
+    override fun done() {
+        try {
+            sc.onSuccess(get())
+        } catch (e: ExecutionException) {
+            ex.onError(e.cause)
+        }
+    }
+}
+
 fun main() {
     val es = Executors.newCachedThreadPool()
 
-    val f = object : FutureTask<String>(
+    val f = CallbackFutureTask(
+        {
+            Thread.sleep(2000)
+            logger.info("Async")
+            "Hello"
+        },
+        {
+            println("Result: $it")
+        },
+        {
+            println("Error: ${it?.message}")
+        }
+    )
+
+    /*val f = object : FutureTask<String>(
         Callable {
             Thread.sleep(2000)
             logger.info("Async")
@@ -23,7 +61,7 @@ fun main() {
         override fun done() {
             println(get())
         }
-    }
+    }*/
 
     /*val f: Future<String> = es.submit<String> {
         Thread.sleep(2000)
@@ -32,6 +70,5 @@ fun main() {
     }*/
 
     es.execute(f)
-    logger.info("Exit")
     es.shutdown()
 }
